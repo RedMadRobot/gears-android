29e9ae6a8cb17dfad00bc13ab7752994240d070a
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-02-28 15:42:18 +0300

    refactor: dependencies -> buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ import com.redmadrobot.build.kotlinCompile
 plugins {
     id("redmadrobot.root-project") version "0.6"
     id("com.github.ben-manes.versions") version "0.36.0"
-    id("dependencies")
     `maven-publish`
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,6 @@ pluginManagement {
 
 rootProject.name = "ktx"
 
-includeBuild("dependencies")
 include(
     "core-ktx",
     "fragment-args-ktx",

02e1ad1bd68f82fdad8632e596ab88f4ef3fc22e
Vitaly Kalenik <v.kalenik@opends.tech>
2021-12-06 13:52:39 +0300

    text: removed unused import

--- resources-ktx/src/main/kotlin/Text.kt
+++ resources-ktx/src/main/kotlin/Text.kt
@@ -3,7 +3,6 @@
 package com.redmadrobot.extensions.resources
 
 import android.content.Context
-import android.os.Parcel
 import android.os.Parcelable
 import android.view.View
 import androidx.annotation.StringRes

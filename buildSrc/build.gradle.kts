9602d0192f5f5a6820a910eeac1e000428eec800
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2022-03-27 17:16:16 +0300

    build: Gradle 7.4.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     `kotlin-dsl`
 }
 
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}
-
 repositories {
     mavenCentral()
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
